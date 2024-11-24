c51b273f4e8f2b75e767b7f34acb60f84517a3a8
omkart <omkartupere7176@gmail.com>
2024-11-24 22:20:44 +0530

    files has been added

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("button1").addEventListener("click", function () {
+  alert("Button 1 clicked!");
+});
+
+document.getElementById("button2").addEventListener("click", function () {
+  alert("Button 2 clicked!");
+});
